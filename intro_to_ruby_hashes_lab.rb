13999fb3d59c6eea2c99cb031964cd416ea47428
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-10-31 20:49:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,7 +36,7 @@ def monopoly_with_third_tier
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
 	     },
-	     :name => {
+	     :names => {
 	       
 	     }
 	   }
@@ -58,7 +58,7 @@ def monopoly_with_fourth_tier
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
 	     },
-	     :name => {
+	     :names => {
 	        :reading_railroad=>
       {
            "mortgage_value"=>"$100"
